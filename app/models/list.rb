48c787a1b74c4d9516435efcc0bd4770ba3573c2
James Miller <james@jkmillertech.com>
2009-01-14 14:58:10 -0800

    Add topics view folder

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ActiveRecord::Base
   has_many :subscriptions
   has_many :subscribers, :through => :subscriptions
-  has_many :threads
+  has_many :topics
   
   validates_presence_of :name
 end
