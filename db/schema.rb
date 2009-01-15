af4f530a42abe40a76e777587063b55466c2490a
funtaff <dph@funtaff.net>
2009-01-14 17:33:38 -0800

    Schema Update

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090114223326) do
+ActiveRecord::Schema.define(:version => 20090114225524) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
