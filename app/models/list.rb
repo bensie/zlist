d773a767928f4c44e7d327224bf9af51805e0b98
James Miller <james@jkmillertech.com>
2009-01-15 13:37:20 -0800

    List model logic for handling new/existing subscribers

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,34 @@ class List < ActiveRecord::Base
   has_many :topics
   
   validates_presence_of :name
+  
+  after_update :save_subscribers
+  
+  def subscribers=(subscribers)
+    
+    # Handle new subscribers
+    if subscribers[:new_subscribers].present?
+      subscribers[:new_subscribers].each do |data|
+        subscribers.build(data) if data[:email].present?
+      end
+    end
+    
+    # Handle existing subscribers
+    subscribers.reject(&:new_record?).each do |data|
+      attributes = subscribers[:existing_subscribers][data.id.to_s]
+      if attributes && attributes[:email].present?
+        data.attributes = attributes
+      else
+        subscriptions.delete(data)
+      end
+    end 
+  end
+  
+  private
+  
+  def save_subscribers
+    subscribers.each do |s|
+      s.save(false)
+    end
+  end
 end
