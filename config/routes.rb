0fe631e70f74c3b08f8639c66b19ef91c2cba73f
funtaff <dph@funtaff.net>
2009-01-14 16:26:36 -0800

    Added map list/send_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :subscribers
-  map.resources :lists
+  map.resources :lists, :member => { :send_test => :get }
   map.root :controller => 'lists', :action => 'index'
+
 end
