f3348b008bab1c852db888b9fc80206d6362fe50
James Miller <james@jkmillertech.com>
2009-01-14 16:36:25 -0800

    Add subscriptions controller

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,41 @@
+class SubscriptionsController < ApplicationController
+  
+  before_filter :find_subscription, :only => %w(show edit update destroy)
+  
+  def index
+    @subscriptions = Subscription.find(:all)
+  end
+
+  def show
+  end
+
+  def new
+    @subscription = Subscription.new
+  end
+
+  def edit
+  end
+
+  def create
+    @subscription = Subscription.create!(params[:subscription])
+    redirect_to :back
+  end
+
+  def update
+    if @subscription.update_attributes(params[:subscription])
+      flash[:notice] = 'Subscription was successfully updated.'
+      redirect_to(@subscription)
+    else
+      render :action => "edit" }
+    end
+  end
+
+  def destroy
+    @subscription.destroy
+    redirect_to :back
+  end
+  
+  def find_subscription
+    @subscription = Subscription.find(params[:id])
+  end
+end
