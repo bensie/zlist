4b4cc53e724d3901654cac10a024f80c65d34cce
James Miller <james@jkmillertech.com>
2009-01-16 19:19:48 -0800

    Some more commenting

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,7 +2,6 @@ class Subscriber < ActiveRecord::Base
   has_many :subscriptions, :dependent => :destroy
   has_many :lists, :through => :subscriptions
 
-  # has_many -> "has written"
   has_many :messages
   
   validates_uniqueness_of :email
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ActiveRecord::Base
+  # This is the table that exists between a list and a subscriber, allowing
+  # their connection to have a state.  We could, for example, add a "disabled"
+  # boolean to this column to give us the ability to temporarily disable a
+  # user's subscription to a list without deleting it.
   belongs_to :list
   belongs_to :subscriber
 end
