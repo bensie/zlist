77d055195cdff1eb80717fd7b80335189baae2d9
James Miller <james@jkmillertech.com>
2009-02-03 17:09:27 -0800

    Add a helper for toggling AJAX checkboxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def toggle_value(object)
+    remote_function(
+      :url => url_for(object),
+      :method => :put,
+      :before => "Element.show('spinner-#{object.id})",
+      :complete => "Element.hide('spinner-#{object.id})",
+      :with => "this.name + '=' + this.checked"
+    )
+  end
 end
