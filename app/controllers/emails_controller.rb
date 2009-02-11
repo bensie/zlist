44d8373b5470344ab1a6f65ba21ce98348dc08d1
James Miller <james@jkmillertech.com>
2009-02-11 11:39:16 -0800

    Add some verification that the server is allowed in

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,9 +1,10 @@
 class EmailsController < ApplicationController
-  # We'll be using server authentication
+  
   skip_before_filter :verify_authenticity_token, :only => %w(create)
   skip_before_filter :login_required, :only => %w(create)
-  # Only admins can send a test mail
+
   before_filter :admin_required, :only => %w(test)
+  before_filter :verify_sender_can_send_email, :only => %w(create)
 
   def create
     # Place holder for real server authentication
@@ -24,5 +25,17 @@ class EmailsController < ApplicationController
 
   # Just for sending a test e-mail
   def test
+  end
+  
+  protected
+  
+  def verify_server_can_send_email
+    server = Server.find_by_key!(params[:key])
+    unless request.env['REMOTE_ADDR'] == server.ip
+      respond_to do |format|
+        format.html { redirect_to root_url }
+        format.xml { render :xml => 'Go pound sand', :status => :unprocessable_entity }
+      end
+    end
   end
 end
