67e0a60666e824279b1daee64f40c147a0fc10d0
James Miller <james@jkmillertech.com>
2009-02-12 15:06:12 -0800

    Move the mail handler yml file contents over to app_config and remove it, update mail_handler.rb to reflect this change

--- lib/mail_handler.rb
+++ lib/mail_handler.rb
@@ -13,13 +13,13 @@ UNAVAILABLE = 69
 # Detect via command line in the future
 debug = true
 
-DEFAULT_TARGET = "http://localhost:3003/emails"
+DEFAULT_TARGET = "http://localhost/emails"
 
 # For now, we'll hard code if no environment is detected
 #ENV['RAILS_ENV'] ||= 'production'
 if( ENV['RAILS_ENV'] )
-  mail_handler_config = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/mail_handler.yml'))[RAILS_ENV].symbolize_keys
-  target_url = mail_handler_config[:target_url]
+  mail_handler_config = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/../config/app_config.yml'))[RAILS_ENV].symbolize_keys
+  target_url = mail_handler_config[:email_target_url]
 else
   target_url = DEFAULT_TARGET
 end
