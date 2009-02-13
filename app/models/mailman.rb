9732a384fec52608d93a95993bd141a6d7145e81
funtaff <dph@funtaff.net>
2009-02-13 11:05:02 -0800

    Cleaned up some comments

--- app/models/mailman.rb
+++ app/models/mailman.rb
@@ -1,29 +1,37 @@
 class Mailman < ActionMailer::Base
   
-  #bcc            Specify the BCC addresses for the message
-  #body           Define the body of the message. This is either a Hash (in which case it specifies the variables to pass to the template when it is rendered), or a string, in which case it specifies the actual text of the message.
-  #cc 	          Specify the CC addresses for the message.
-  #charset 	      Specify the charset to use for the message. This defaults to the default_charset specified for ActionMailer::Base.
-  #content_type 	Specify the content type for the message. This defaults to <text/plain in most cases, but can be automatically set in some situations.
-  #from 	        Specify the from address for the message.
-  #reply_to 	    Specify the address (if different than the “from” address) to direct replies to this message.
-  #headers 	      Specify additional headers to be added to the message.
-  #implicit_parts_order 	Specify the order in which parts should be sorted, based on content-type. This defaults to the value for the default_implicit_parts_order.
-  #mime_version 	Defaults to “1.0”, but may be explicitly given if needed.
-  #recipient 	    The recipient addresses for the message, either as a string (for a single address) or an array (for multiple addresses).
-  #sent_on 	      The date on which the message was sent. If not set (the default), the header will be set by the delivery agent.
-  #subject 	      Specify the subject of the message.
-  #template 	    Specify the template name to use for current message. This is the “base” template name, without the extension or directory, and may be used to have multiple mailer methods share the same template.
+#   bcc       Specify the BCC addresses for the message
+#   body      Define the body of the message. This is either a Hash 
+#             (in which case it specifies the variables to pass to the template when it is rendered), 
+#             or a string, in which case it specifies the actual text of the message.
+#   cc 	      Specify the CC addresses for the message.
+#   charset   Specify the charset to use for the message. This defaults to the default_charset specified for ActionMailer::Base.
+#   content_type 	Specify the content type for the message. This defaults to <text/plain in most cases, 
+#                   but can be automatically set in some situations.
+#   from 	        Specify the from address for the message.
+#   reply_to 	    Specify the address (if different than the “from” address) to direct replies to this message.
+#   headers 	      Specify additional headers to be added to the message.
+#   implicit_parts_order 	Specify the order in which parts should be sorted, based on content-type. 
+#                           This defaults to the value for the default_implicit_parts_order.
+#   mime_version 	Defaults to “1.0”, but may be explicitly given if needed.
+#   recipient 	    The recipient addresses for the message, either as a string (for a single address) 
+#                   or an array (for multiple addresses).
+#   sent_on 	    The date on which the message was sent. If not set (the default), the 
+#                   header will be set by the delivery agent.
+#   subject 	    Specify the subject of the message.
+#   template 	    Specify the template name to use for current message. This is the “base” template name, 
+#                   without the extension or directory, and may be used to have multiple mailer methods share the same template.
 
   # Method for processing incoming messages
-  # pre: email (as passed from ActionMailer receieve) 
+  # pre: (Tmail email)
   def receive(email)
     # TODO: clean up with regexp and logic 
 
     s_pre, s_domain = email.to.first.split(/\@/)
     s_list, s_topic = s_pre.split(/\+/)
 
-    # Don't storm
+    # Don't storm if using BCC method with To: noreply 
+    # TODO: remove 
     if(s_list == "mailer" || s_list == "noreply")
       exit
     end
@@ -80,7 +88,7 @@ class Mailman < ActionMailer::Base
   end
 
   # Send a test e-mail to everyone on a given list
-  # pre: list (a List object) 
+  # pre: (List list)
   def list_test_dispatch(list)
     list.subscribers.each do |subscriber|
       #recipients  "noreply@" + APP_CONFIG[:email_domain]
@@ -95,7 +103,7 @@ class Mailman < ActionMailer::Base
   protected
 
   # Response to a message posted to a list that doesn't exist
-  # pre: email (as passed from ActionMailer receieve) 
+  # pre: (TMail email)
   def no_such_list(email)
     recipients  email.from
     from        "#{ APP_CONFIG[:email_domain] } <mailer@#{ APP_CONFIG[:email_domain] }>"
@@ -103,7 +111,7 @@ class Mailman < ActionMailer::Base
   end
 
   # Response to a message posted in reply to a tpoic that doesn't exist
-  # pre: email (as passed from ActionMailer receieve) 
+  # pre: (List list, Tmail email)
   def no_such_topic(list, email)
     recipients  email.from
     from        "#{ APP_CONFIG[:email_domain] } <mailer@#{ APP_CONFIG[:email_domain] }>"
@@ -111,6 +119,8 @@ class Mailman < ActionMailer::Base
     body        :list => email.list.name
   end
 
+  # Response to a message sent to a noreply address
+  # pre: (Tmail email)
   def no_reply_address(email)
     recipients  email.from
     from        "#{ APP_CONFIG[:email_domain] } <mailer@#{ APP_CONFIG[:email_domain] }>"
@@ -120,7 +130,7 @@ class Mailman < ActionMailer::Base
   end
 
   # Reponse to a message posted to a list by a non-member
-  # pre: email (as passed from ActionMailer receieve) 
+  # pre: (List list, Tmail email)
   def cannot_post(list, email)
     recipients  email.from
     from        "#{ APP_CONFIG[:email_domain] } <mailer@#{ APP_CONFIG[:email_domain] }>"
@@ -129,7 +139,7 @@ class Mailman < ActionMailer::Base
   end
 
   # Send an e-mail out to a list
-  # pre: email (as passed from ActionMailer receieve) 
+  # pre: (Topic topic, Tmail email, Subscriber subscriber)
   def to_mailing_list(topic, email, subscriber)
     recipients  subscriber.name + " <#{subscriber.email}>"
     from        "#{email.from} <mailer@#{ APP_CONFIG[:email_domain] }>"
