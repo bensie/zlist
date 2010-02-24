e49fe862b2fe7da48858fb768c65c0f366583b76
James Miller <james@jkmillertech.com>
2010-02-23 16:27:23 -0800

    Place nice with PG

--- db/migrate/20090203193936_add_disabled_to_subscriber.rb
+++ db/migrate/20090203193936_add_disabled_to_subscriber.rb
@@ -1,6 +1,6 @@
 class AddDisabledToSubscriber < ActiveRecord::Migration
   def self.up
-    add_column :subscribers, :disabled, :boolean, :default => 0
+    add_column :subscribers, :disabled, :boolean, :default => false
   end
 
   def self.down
