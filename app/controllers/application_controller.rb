32b13626f8bc9704f8e9b8f805cd68eb9724648c
James Miller <bensie@gmail.com>
2011-05-25 14:43:23 -0700

    Several rails 3 syntax changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,18 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  helper :all # include all helpers, all the time
 
   protect_from_forgery
-  filter_parameter_logging :password, :password_confirmation
-  
+
   before_filter :login_required
-  
+
   private
-  
+
   def admin_required
     unless admin?
       redirect_to root_url
     end
   end
-  
+
   def admin?
     logged_in? && current_user.admin?
   end
--- lib/authentication.rb
+++ lib/authentication.rb
@@ -2,15 +2,15 @@ module Authentication
   def self.included(controller)
     controller.send :helper_method, :current_user, :logged_in?
   end
-  
+
   def current_user
     @current_user ||= Subscriber.find(session[:subscriber_id]) if session[:subscriber_id]
   end
-  
+
   def logged_in?
     current_user
   end
-  
+
   def login_required
     unless logged_in?
       flash[:warning] = "You must log in or sign up before accessing this page."
@@ -18,15 +18,15 @@ module Authentication
       redirect_to login_url
     end
   end
-  
+
   def redirect_to_target_or_default(default)
     redirect_to(session[:return_to] || default)
     session[:return_to] = nil
   end
-  
+
   private
-  
+
   def store_target_location
-    session[:return_to] = request.request_uri
+    session[:return_to] = request.url
   end
 end
