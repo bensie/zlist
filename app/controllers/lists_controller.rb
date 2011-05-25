f29d5c3eaa654f1b5fbe6e4583279c664e4f3b4c
James Miller <bensie@gmail.com>
2011-05-25 16:48:49 -0700

    Use rails 3 controller syntax

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,19 +1,21 @@
 class ListsController < ApplicationController
-  
+
   before_filter :admin_required, :except => %w(index show subscribe unsubscribe available)
   before_filter :find_list, :only => %w(edit send_test update destroy subscribe unsubscribe)
 
+  respond_to :html, :js
+
   def index
-    @lists = admin? ? List.all(:include => :subscribers, :order => :name) : current_user.lists(:order => :name)
+    @lists = admin? ? List.includes(:subscribers).order(:name) : current_user.lists.order(:name)
   end
-  
+
   def available
-    @lists = List.public(:order => :name)
+    @lists = List.public.order(:name)
   end
 
   def show
     @list = List.find(params[:id])
-    @subscriptions = @list.subscriptions.all(:include => :subscriber, :order => 'subscribers.name')
+    @subscriptions = @list.subscriptions.includes(:subscriber).order('subscribers.name')
     @subscription = Subscription.new
     @available_subscribers = Subscriber.find_subscribers_not_in_list(@list.id)
   end
@@ -26,30 +28,18 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(params[:list])
-    if @list.save
-      flash[:notice] = 'List was successfully created.'
-      redirect_to @list
-    else
-      flash.now[:warning] = 'There was a problem creating the list.'
-      render "new"
-    end
+    @list = List.create(params[:list])
+    respond_with @list
   end
 
   def update
-    if @list.update_attributes(params[:list])
-      flash[:notice] = 'List was successfully updated.'
-      redirect_to @list
-    else
-      flash.now[:warning] = 'There was a problem updating the list.'
-      render "edit" 
-    end
+    @list.update_attributes(params[:list])
+    respond_with @list
   end
 
   def destroy
     @list.destroy
-    flash[:notice] = "The list was deleted."
-    redirect_to lists_url
+    respond_with @list
   end
 
   def send_test
@@ -70,7 +60,7 @@ class ListsController < ApplicationController
   end
 
   private
-  
+
   def find_list
     @list = List.find(params[:id])
   end
