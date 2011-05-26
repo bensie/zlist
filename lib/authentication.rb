c3423446d3f5be792e37035433df53f42c0242c6
James Miller <bensie@gmail.com>
2011-05-26 09:39:45 -0700

    Minor updates to authentication

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,6 +1,6 @@
 module Authentication
   def self.included(controller)
-    controller.send :helper_method, :current_user, :logged_in?
+    controller.send :helper_method, :current_user, :logged_in?, :redirect_to_target_or_default
   end
 
   def current_user
@@ -13,14 +13,13 @@ module Authentication
 
   def login_required
     unless logged_in?
-      flash[:warning] = "You must log in or sign up before accessing this page."
       store_target_location
-      redirect_to login_url
+      redirect_to login_url, :alert => "You must first log in or sign up before accessing this page."
     end
   end
 
-  def redirect_to_target_or_default(default)
-    redirect_to(session[:return_to] || default)
+  def redirect_to_target_or_default(default, *args)
+    redirect_to(session[:return_to] || default, *args)
     session[:return_to] = nil
   end
 
