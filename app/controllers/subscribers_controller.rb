6b4975db33183f6bbe0fe12cdaaddbec0435f2a1
James Miller <bensie@gmail.com>
2011-05-25 22:16:47 -0700

    Clean up views, indentation and such

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,17 +1,17 @@
 class SubscribersController < ApplicationController
-  
+
   skip_before_filter :login_required, :only => %w(new create)
   before_filter :admin_required, :only => %w(index destroy disable toggle_administrator)
   before_filter :find_subscriber, :only => %w(show edit update destroy disable toggle_administrator)
-  
+
   def index
-    @subscribers = Subscriber.active.paginate(:page => params[:page])
+    @subscribers = Subscriber.active.page(params[:page])
   end
-  
+
   def search
     # Search term
     term = params[:term].to_s
-    
+
     # Don't show search results with an empty 'term' or it will return a ton of non-paginated records
     unless term.empty?
       @subscribers = Subscriber.search(term).all
@@ -19,7 +19,7 @@ class SubscribersController < ApplicationController
       # Make the @subscribers array empty for when the search term is blank, we don't want it returning all records
       @subscribers = []
     end
-    
+
     respond_to do |format|
       format.js
       format.html do
@@ -83,21 +83,21 @@ class SubscribersController < ApplicationController
     end
     redirect_to(subscribers_url)
   end
-  
+
   def disable
     @subscriber.disable
     redirect_to(subscribers_url)
   end
-  
+
   def toggle_administrator
     @subscriber.update_attribute(:admin, params[:subscriber][:admin]) unless @subscriber == current_user
     respond_to do |format|
       format.js { head :ok }
     end
   end
-  
+
   private
-  
+
   def find_subscriber
     if admin?
       @subscriber = Subscriber.find(params[:id])
