07b52de0c4ee0c1764bfbedcdb61fb6a6b0ee3bd
James Miller <bensie@gmail.com>
2012-01-30 19:41:12 -0800

    Dummy

--- app/workers/inbound_email_processor.rb
+++ app/workers/inbound_email_processor.rb
@@ -2,7 +2,7 @@ class InboundEmailProcessor
   @queue = :email
 
   def self.perform(json)
-    hash = ActiveSupport::JSON.decode(request.body)
+    hash = ActiveSupport::JSON.decode(json)
     Inbound::Email.new(hash).process
   end
 end
